ed78cf3c96dbd80df4fc3b7f2cd0ea8c76913664
XpertiCON106 <tahalil106@gmail.com>
2025-02-25 00:18:08 -0500

    basic setup

---
+++ button_counter/src/app/component/Footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <div>
+           This is the footer component
+        </div>
+    );
+};
+
+export default Footer;
