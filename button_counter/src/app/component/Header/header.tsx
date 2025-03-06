6a71c47370b0e4c58e777f2a946dba0aba69a6a4
XpertiCON106 <tahalil106@gmail.com>
2025-03-05 23:36:29 -0500

    header setup

--- button_counter/src/app/component/Header/header.tsx
+++ button_counter/src/app/component/Header/header.tsx
@@ -1,11 +1,32 @@
+// Header.js
+
+import Link from 'next/link';
 import React from 'react';
+import styles from './header.module.css'; // Import the CSS module
 
 const Header = () => {
-    return (
-        <header>
-            <h1>This is the header component</h1>
-        </header>
-    );
+  const navItems = [
+    { title: 'Home', href: '/home' },
+    { title: 'Create an account', href: '/create-account' },
+  ];
+
+  return (
+    <header className={styles.header}>
+      <nav>
+        <div className={styles.navListWrapper}>
+          <ul className={styles.navList}>
+            {navItems.map((item) => (
+              <li key={item.href}>
+                <Link href={item.href} className={styles.navLink}>
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
